361d63f4c163f42ff88e346cee6fa0a4e61ed4ca
kimwoojin87 <gregorirr@gmail.com>
2019-07-01 17:34:24 +0900

    07-01

--- cafe/src/main/java/com/kitri/cafe/board/dao/MemoDao.java
+++ cafe/src/main/java/com/kitri/cafe/board/dao/MemoDao.java
@@ -7,9 +7,9 @@ import com.kitri.cafe.board.model.MemoDto;
 
 public interface MemoDao {
 	
-	int writeArticle(MemoDto memoDto);
-	List<MemoDto> listArticle(Map<String, String> parameter);
-	MemoDto viewArticle(int seq);
-	int modifyArticle(MemoDto memoDto);
-	void deleteArticle(int seq);
+	int writeMemo(MemoDto memoDto);
+	List<MemoDto> listMemo(int seq);
+	MemoDto viewMemo(int seq);
+	int modifyMemo(MemoDto memoDto);
+	void deleteMemo(int mseq);
 }
--- cafe/src/main/java/com/kitri/cafe/board/service/MemoService.java
+++ cafe/src/main/java/com/kitri/cafe/board/service/MemoService.java
@@ -1,17 +1,12 @@
 package com.kitri.cafe.board.service;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-
 import com.kitri.cafe.board.model.MemoDto;
 
 public interface MemoService {
-	
+
 	void writeMemo(MemoDto memoDto);
-	List<MemoDto> listMemo(Map<String, String> parameter);
+	String listMemo(int seq);
 	void modifyMemo(MemoDto memoDto);
-	void deleteMemo(int mseq);
+	String deleteMemo(int seq, int mseq);
+	
 }
